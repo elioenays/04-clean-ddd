72ba8ec26f743ffc6064d85fc6b2a470a82f1123
Elioenay Silva <elionay407@gmail.com>
2023-05-05 17:05:02 -0300

    first commit

---
+++ src/domain/use-cases/answer-question.ts
@@ -0,0 +1,28 @@
+import { UniqueEntityID } from '../../core/entities/unique-entity-id'
+import { Answer } from '../entities/answer'
+import { AnswersRepository } from '../repositories/answers-repository'
+
+interface AnswerQuestionUseCaseRequest {
+  content: string
+  instructorId: string
+  questionId: string
+}
+
+export class AnswerQuestionUseCase {
+  constructor(private readonly answersRepository: AnswersRepository) {}
+  async execute({
+    content,
+    instructorId,
+    questionId,
+  }: AnswerQuestionUseCaseRequest) {
+    const answer = Answer.create({
+      content,
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
+    })
+
+    await this.answersRepository.create(answer)
+
+    return answer
+  }
+}
