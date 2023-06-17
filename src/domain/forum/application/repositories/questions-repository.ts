2c34297d74825f0fdcefdf9a113190f0c173f850
Elioenay <elionay407@gmail.com>
2023-06-16 23:46:34 -0300

    Caso de uso: Deletar pergunta

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -2,5 +2,7 @@ import { Question } from '@/domain/forum/enterprise/entities/question'
 
 export interface QuestionsRepository {
   findBySlug(slug: String): Promise<Question | null>
+  findById(id: String): Promise<Question | null>
   create(question: Question): Promise<void>
+  delete(question: Question): Promise<void>
 }
