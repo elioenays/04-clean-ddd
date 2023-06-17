11522bb7e1623ce776fa8a0dbeac32485151e100
Elioenay <elionay407@gmail.com>
2023-06-17 19:31:30 -0300

    Caso de uso: Editar pergunta

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -2,4 +2,6 @@ import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
 export interface AnswersRepository {
   create(answer: Answer): Promise<void>
+  delete(answer: Answer): Promise<void>
+  findById(id: String): Promise<Answer | null>
 }
--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -5,6 +5,22 @@ import { Answer } from '@/domain/forum/enterprise/entities/answer'
 export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = []
 
+  async delete(answer: Answer): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items.splice(itemIndex, 1)
+  }
+
+  async findById(id: String): Promise<Answer | null> {
+    const answer = this.items.find((item) => item.id.toString() === id)
+
+    if (!answer) {
+      return null
+    }
+
+    return answer
+  }
+
   async create(answer: Answer) {
     this.items.push(answer)
   }
