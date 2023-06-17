61964c14b7edacfb0d7e464142a46714e5513eec
Elioenay <elionay407@gmail.com>
2023-06-16 22:30:42 -0300

    Caso de uso: Criar pergunta

---
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -0,0 +1,20 @@
+import { QuestionsRepository } from '../repositories/questions-repository'
+import { Question } from '../../enterprise/entities/question'
+import { CreateQuestionUseCase } from './create-question'
+
+const fakeQuestionsRepository: QuestionsRepository = {
+  create: async (question: Question) => {},
+}
+
+test('create an question', async () => {
+  const createQuestion = new CreateQuestionUseCase(fakeQuestionsRepository)
+
+  const { question } = await createQuestion.execute({
+    authorId: '1',
+    content: 'Conteudo da pergunta',
+    title: 'New question',
+  })
+
+  expect(question.id).toBeTruthy()
+  expect(question.content).toEqual('Conteudo da pergunta')
+})
