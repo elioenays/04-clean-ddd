c74ee9bc1e368e868e90938d336771b6446a6a85
Elioenay <elionay407@gmail.com>
2023-06-16 23:08:24 -0300

    Gerando dados fictícios

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -3,16 +3,21 @@ import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
-import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import { faker } from '@faker-js/faker'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    title: 'Example question',
-    slug: Slug.create('example-question'),
-    authorId: new UniqueEntityID(),
-    content: 'Question content',
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
