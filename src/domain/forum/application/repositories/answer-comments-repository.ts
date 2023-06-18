bb8cbaaf2da66496c8998441726f24a6336f6df0
Elioenay <elionay407@gmail.com>
2023-06-17 22:17:12 -0300

    Caso de uso: Deletar comentário da resposta

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,5 +1,7 @@
 import { AnswerComment } from '../../enterprise/entities/answer-comment'
 
 export interface AnswerCommentsRepository {
+  findById(id: String): Promise<AnswerComment | null>
   create(answerComment: AnswerComment): Promise<void>
+  delete(answerComment: AnswerComment): Promise<void>
 }
--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -4,6 +4,24 @@ import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment
 export class InMemoryAnswerCommentsRepository
   implements AnswerCommentsRepository
 {
+  async findById(id: string): Promise<AnswerComment | null> {
+    const answerComment = this.items.find((item) => item.id.toString() === id)
+
+    if (!answerComment) {
+      return null
+    }
+
+    return answerComment
+  }
+
+  async delete(answerComment: AnswerComment): Promise<void> {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === answerComment.id,
+    )
+
+    this.items.splice(itemIndex, 1)
+  }
+
   public items: AnswerComment[] = []
 
   async create(answer: AnswerComment) {
