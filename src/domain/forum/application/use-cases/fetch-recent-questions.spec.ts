d44f4935bd4b37e3a4b626210e6d59d752d5a542
Elioenay <elionay407@gmail.com>
2023-06-19 23:11:01 -0300

    Excluindo anexos da pergunta

--- src/domain/forum/application/use-cases/fetch-recent-questions.spec.ts
+++ src/domain/forum/application/use-cases/fetch-recent-questions.spec.ts
@@ -1,13 +1,20 @@
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 import { makeQuestion } from 'test/factories/make-question'
 import { FetchQuestionsUseCase } from './fetch-recent-questions'
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
 let sut: FetchQuestionsUseCase
 
 describe('Fetch Recent questions', () => {
   beforeEach(() => {
-    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryQuestionAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository()
+
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionAttachmentsRepository,
+    )
     sut = new FetchQuestionsUseCase(inMemoryQuestionsRepository)
   })
 
