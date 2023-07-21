e53a30ff3be3fdabde8747b9849101add077edad
Elioenay <elionay407@gmail.com>
2023-07-21 10:14:11 -0300

    Subdomínios & Domain Events - Caso de uso: ler notificação

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -6,6 +6,24 @@ export class InMemoryNotificationsRepository
 {
   public items: Notification[] = []
 
+  async save(notification: Notification) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === notification.id,
+    )
+
+    this.items[itemIndex] = notification
+  }
+
+  async findById(id: String) {
+    const notification = this.items.find((item) => item.id.toString() === id)
+
+    if (!notification) {
+      return null
+    }
+
+    return notification
+  }
+
   async create(notification: Notification) {
     this.items.push(notification)
   }
