dbcfe49037071fb0f0c2146dec0b044084cd3196
Elioenay <elionay407@gmail.com>
2023-07-24 08:57:04 -0300

    Testando eventos de domínio

---
+++ src/core/events/domain-events.spec.ts
@@ -0,0 +1,44 @@
+import { AggregateRoot } from '../entities/aggregate-root'
+import { UniqueEntityID } from '../entities/unique-entity-id'
+import { DomainEvent } from './domain-event'
+import { DomainEvents } from './domain-events'
+
+class CustomAggregate extends AggregateRoot<any> {
+  static create() {
+    const aggregate = new CustomAggregate(null)
+
+    aggregate.addDomainEvent(new CustomAggregateCreated(aggregate))
+
+    return aggregate
+  }
+}
+
+class CustomAggregateCreated implements DomainEvent {
+  public ocurredAt: Date
+  private aggregate: CustomAggregate
+
+  constructor(aggregate: CustomAggregate) {
+    this.ocurredAt = new Date()
+    this.aggregate = aggregate
+  }
+
+  public getAggregateId(): UniqueEntityID {
+    return this.aggregate.id
+  }
+}
+
+describe('domain events', () => {
+  it('should be able to dispatch and listen to events', () => {
+    const callBackSpy = vi.fn()
+    DomainEvents.register(callBackSpy, CustomAggregateCreated.name)
+
+    const aggregate = CustomAggregate.create()
+
+    expect(aggregate.domainEvents).toHaveLength(1)
+
+    DomainEvents.dispatchEventsForAggregate(aggregate.id)
+
+    expect(callBackSpy).toHaveBeenCalled()
+    expect(aggregate.domainEvents).toHaveLength(0)
+  })
+})
